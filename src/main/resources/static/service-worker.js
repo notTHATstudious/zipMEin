46d13782e32fee9ab9db3faed860031be6b1f02e
notTHATstudious <mahi17esh@gmail.com>
2026-04-27 21:31:24 +0530

    V2: PWA support + mobile UI + dark/light mode

---
+++ src/main/resources/static/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "zipmein-static-v1";
+const STATIC_ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).catch(() => caches.match("/index.html"));
+    })
+  );
+});
